955248b8c145b040c4fcc6c0d1c3e2393ee4749e
aeckumich <aeckr@umich.edu>
2024-08-11 19:37:47 -0400

    hiding api key

--- hidden_sample.py
+++ hidden_sample.py
@@ -1,3 +1,9 @@
-#personal
+"""This is a sample of how to set up your hidden file."""
+
+#Spotify Creds
 CLIENT_ID = "YOUR CLIENT ID"
 CLIENT_SECRET = "YOUR CLIENT SECRET"
+
+#Open AI API Key: 
+OpenAI_api_key = 'Your API Key'
+
